e1791326ea690a5c3a192c88ef4d43ae582b605f
Mario <mariosantos88477364484@gmail.com>
2024-01-15 19:24:40 -0300

    Countdown

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -86,7 +86,7 @@ export const Separator = styled.div`
 	justify-content: center;
 `;
 
-export const StartCountdownButton = styled.button`
+export const BaseCountdownButton = styled.button`
 	width: 100%;
 	border: 0;
 	padding: 1rem;
@@ -95,6 +95,7 @@ export const StartCountdownButton = styled.button`
 	display: flex;
 	align-items: center;
 	justify-content: center;
+	color: ${({ theme }) => theme["gray-100"]};
 
 	gap: 0.5rem;
 
@@ -102,15 +103,24 @@ export const StartCountdownButton = styled.button`
 
 	cursor: pointer;
 
-	background: ${({ theme }) => theme["green-500"]};
-	color: ${({ theme }) => theme["gray-100"]};
-
 	&:disabled {
 		opacity: 0.7;
 		cursor: not-allowed;
 	}
+`;
+
+export const StartCountdownButton = styled(BaseCountdownButton)`
+	background: ${({ theme }) => theme["green-500"]};
 
 	&:not(:disabled):hover {
 		background: ${({ theme }) => theme["green-700"]};
+	}
+`;
+
+export const StopCountdownButton = styled(BaseCountdownButton)`
+	background: ${({ theme }) => theme["red-500"]};
+
+	&:not(:disabled):hover {
+		background: ${({ theme }) => theme["red-700"]};
 	}
 `;
