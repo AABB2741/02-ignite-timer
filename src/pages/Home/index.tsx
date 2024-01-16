5fec2da8d8b2bb02c214d113bf7f63a543202158
Mario <mariosantos88477364484@gmail.com>
2024-01-16 11:30:02 -0300

    Add history

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -36,7 +36,12 @@ export function Home() {
 		},
 	});
 
-	const { handleSubmit, watch /* reset */ } = newCycleForm;
+	const { handleSubmit, watch, reset } = newCycleForm;
+
+	function handleCreateNewCycle(data: NewCycleFormData) {
+		createNewCycle(data);
+		reset();
+	}
 
 	const task = watch("task");
 	const isSubmitDisabled = !task;
@@ -47,7 +52,7 @@ export function Home() {
 
 	return (
 		<HomeContainer>
-			<form onSubmit={handleSubmit(createNewCycle)}>
+			<form onSubmit={handleSubmit(handleCreateNewCycle)}>
 				<FormProvider {...newCycleForm}>
 					<NewCycleForm />
 				</FormProvider>
